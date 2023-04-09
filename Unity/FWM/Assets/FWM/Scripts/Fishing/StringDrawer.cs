9135f3d1b1d6772165998054e46c3fa422d8c0bb
PhiOzoa <f_timcik@iinet.net.au>
2023-04-09 16:00:54 +0800

    basic fishing line implemented

--- Unity/FWM/Assets/FWM/Scripts/Fishing/StringDrawer.cs
+++ Unity/FWM/Assets/FWM/Scripts/Fishing/StringDrawer.cs
@@ -6,6 +6,21 @@ namespace Fishing
 {
     public class StringDrawer : MonoBehaviour
     {
+		public LineRenderer fishingLine;
+		public GameObject cam;
 		
+		private Vector3 hookDir;
+		
+		private Vector3 lineTopPos = Vector3.forward;
+		
+		private void FixedUpdate()
+		{
+			hookDir = new Vector3(gameObject.transform.position.x, 0f, gameObject.transform.position.z).normalized;
+			
+			lineTopPos = Vector3.Lerp(lineTopPos, cam.transform.position + hookDir, Time.deltaTime);
+			
+			fishingLine.SetPosition(0, gameObject.transform.position + (Vector3.up / 10f));
+			fishingLine.SetPosition(1, lineTopPos);
+		}
     }
 }
